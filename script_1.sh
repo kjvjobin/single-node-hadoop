ebca1303684a04d6a8ac2e2354095e4f217c4040
Jobin <43548644+jobinkeecheril@users.noreply.github.com>
2020-08-15 18:55:00 +0530

    Update script1
    sed for sudo user

--- script_1.sh
+++ script_1.sh
@@ -4,7 +4,7 @@ whoami
 addgroup hadoop
 adduser hadoopuser
 adduser hadoopuser hadoop
-#sed '34 hadoopuser ALL= (ALL:ALL) NOPASSWD:ALL' /etc/sudoers
+sed '34 hadoopuser ALL= (ALL:ALL) NOPASSWD:ALL' /etc/sudoers
 sudo cat <<EOF | tee >> /etc/sudoers
 hadoopuser     ALL=(ALL:ALL)   NOPASSWD:ALL
 EOF
